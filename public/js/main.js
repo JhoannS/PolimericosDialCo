1fc50efaac108b3b5720061dfbaf0d911c97c659
JhoannS <sebastianzamudio2004@gmail.com>
2024-05-21 12:08:32 -0500

    Configuracion de entornos para la base de datos, MAY 21 12:10 PM

--- public/js/main.js
+++ public/js/main.js
@@ -1,60 +1,3 @@
-// const popupOverlay = document.querySelector(".popup-overlay");
-// const skipButton = document.querySelector(".popup-container .skip-button");
-// const visitButton = document.querySelector(".popup-container .visit-button");
-
-// let remainingTime = 3;
-// let allowedToSkip = false;
-// let popupTimer;
-
-// const createPopupCookie = () => {
-//   let expiresDays = 30;
-//   let date = new Date();
-//   date.setTime(date.getTime() + expiresDays * 6 * 60 * 60 * 1000);
-//   let expires = "expires=" + date.toUTCString();
-//   document.cookie = `popupCookie=true; ${expires}; path=/;`;
-// };
-
-// const showAd = () => {
-//   popupOverlay.classList.add("active");
-//   popupTimer = setInterval(() => {
-//     skipButton.innerHTML = `Omitir en ${remainingTime}s`;
-//     remainingTime--;
-
-//     if (remainingTime < 0) {
-//       allowedToSkip = true;
-//       skipButton.innerHTML = "Omitir";
-//       clearInterval(popupTimer);
-//     }
-//   }, 1000);
-// };
-
-// const skipAd = () => {
-//   popupOverlay.classList.remove("active");
-//   createPopupCookie();
-// };
-
-// skipButton.addEventListener("click", () => {
-//   if (allowedToSkip) {
-//     skipAd();
-//   }
-// });
-
-// const startTimer = () => {
-//   if (window.scrollY > 100) {
-//     showAd();
-//     window.removeEventListener("scroll", startTimer);
-//   }
-// };
-
-// if (!document.cookie.match(/^(.*;)?\s*popupCookie\s*=\s*[^;]+(.*)?$/)) {
-//   window.addEventListener("scroll", startTimer);
-// }
-
-// window.addEventListener("scroll", function () {
-//   const header = document.querySelector("header");
-//   header.classList.toggle("abajo", window.scrollY > 0);
-// });
-
 // scripts.js de portada
 const contenido = document.querySelector('aside');
 let lastScrollTop = 0;
@@ -62,7 +5,7 @@ let lastScrollTop = 0;
 function mostrarContenido() {
     const alturaVentana = window.innerHeight;
     const distanciaContenido = contenido.getBoundingClientRect().top;
-    const st = window.pageYOffset || document.documentElement.scrollTop;
+    const st = window || document.documentElement.scrollTop;
 
     if (st > lastScrollTop) {
         // Scroll hacia abajo
