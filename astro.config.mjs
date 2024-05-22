cb4edca7a692081f4e9a4cc00e214e1f3cd38911
JhoannS <sebastianzamudio2004@gmail.com>
2024-05-22 08:59:07 -0500

    Instalacion de configuracion par despligue en vercel, MAY 22 9:01 AM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+import cookie from "cookie"; 
 
 import db from "@astrojs/db";
 
